f741541c13b54670e5965a3493f18455d7cf36a8
Jeffrey Nyauke <jeffnyauke@gmail.com>
2023-05-29 20:03:43 +0300

    Fix sample

--- samples/ktor/build.gradle.kts
+++ samples/ktor/build.gradle.kts
@@ -50,5 +50,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation(projects.lib.mpesaKmpLibraryCore)
+    implementation(project(":lib:mpesa-kmp-library-core"))
 }
